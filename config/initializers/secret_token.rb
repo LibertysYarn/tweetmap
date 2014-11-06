c7066ba9f9809a97454170ccf08024b214ab2cd6
LibertysYarn <code@libertysyarn.com>
2014-11-05 20:58:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetmap::Application.config.secret_key_base = '44bc40e64355f658cd163955f0460201714e071196033f069f833b022d0028b0deb579251db4715dcf36035d81aefdc241c45a3994055da213dca60389c98f82'
